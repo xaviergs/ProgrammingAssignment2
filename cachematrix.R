e89283c1ae2a6a2caee2ec61bfc3943dd909fbc8
xaviergs <xagasb@gmail.com>
2014-05-24 11:14:44 +0200

    Coursera R programming. Assignment 2 R script
    Scripts requested to create a cached matrix object and evaluate its
    inverse.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,85 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This set of functions perform the next tasks:
+## makeCacheMatrix: Creates a "matrix-object" that gets and sets
+##      a object  and provides two methods for getting and setting
+##      its inverse. Uses the <<- operator to cache the object in 
+##      the parent environment, so it will persist and can be queried
+##      from different calls to the matrix-object
+## cacheSolve: Uses the makeCacheMatrix object to evaluate the inverse
+##      of a matrix. Has the hability of querying the cache to know
+##      if the inverse object alredy exists and avoid time consuming 
+##      calcualtions. Also, it provides an starting check of feasibility
+##      of the inverse matrix operation by verifying its dimensions
 
-## Write a short comment describing this function
+##############################################################
+##      makeCacheMatrix -> creates a matrix-object composed by data and
+##      methods to query it
+makeCacheMatrix <- function(x = matrix()){
+        ##      Initializes the cached working matrix
+        cachedMatrix <- NULL
+        ## Internal functions
+        ## set: stores the passed matrix
+        set <- function(y){
+                x <<- y
+                cachedMatrix <<- NULL
+        }
+        ## get: returns the working matrix
+        get <- function() x
+        ## setInverse: evaluates the inverse of the
+        ##      working matrix using the "solve" R function
+        setInverse <- function(solve) cachedMatrix <<- solve
+        ## getInverse: returns the matrix inverse
+        getInverse <- function() cachedMatrix
+        
+        ## Returns a list containing the four useful functions
+        ##      for the cached matrix object
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
+}
 
-makeCacheMatrix <- function(x = matrix()) {
-
+##############################################################
+##      cacheSolve -> caches the inverse of a matrix-object using
+##      both the internal methods and the hability of accessing
+##      the parent environment
+cacheSolve <- function(x,...){
+        ##      Gets the value for the inverse in the working structure
+        invMatrix <- x$getInverse()
+        ##      When cached inverse exists, shows the message
+        ##      and returns the cached value
+        if(!is.null(invMatrix)){
+                message("Info.: Getting cached data")
+                return(invMatrix)
+        }
+        
+        ##      Gets the working structure
+        workMatrix <- x$get()
+        ##      Verifies that the working matrix is an square one, 
+        ##      otherwise the inverse cannot be
+        ##      calculated
+        if(nrow(workMatrix) != ncol(workMatrix)){
+                message("Error: Only squared matrices can be inverted.")
+                message(paste("Can't perform the calculation, the argument passed is a (",
+                              toString(nrow(workMatrix))," row X ", 
+                              toString(ncol(workMatrix))," col) matrix",
+                              sep =""))
+                return(workMatrix)
+        }
+        ##      Shows a warning informing that the operation
+        ##      requested can take some time
+        message("Warning: Calculating inverse, it can take a while...")
+        tStart <- Sys.time()
+        ##      Applies the structure function to get the inverse
+        invMatrix <- solve(workMatrix, ...)
+        tEnd <- Sys.time()
+        ##      Shows an info message with the time taken to perform the calcualtion.
+        message(paste("Info: Inverse matrix calculation took:",
+                      toString(round(tEnd - tStart,3)),
+                      " sec.",
+                      sep = ""))
+        ##      Sets the inverse into the structure's cache
+        x$setInverse(invMatrix)
+        ##      returns the inverse
+        invMatrix
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
